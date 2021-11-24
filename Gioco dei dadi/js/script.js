4c896973a8ba63efe7c60b60446b377a810bd139
MemeWasTaken <memelazzari02@gmail.com>
2021-11-24 15:58:04 +0100

    Gioco dei dadi: problem in words

---
+++ Gioco dei dadi/js/script.js
@@ -0,0 +1,4 @@
+// 1) Generare numero casuale da 1 a 6 per il giocatore
+// 2) Generare numero casuale da 1 a 6 per il computer
+// 3) Verificare quale dei due numeri usciti è maggiore
+// 4) printInConsole 
